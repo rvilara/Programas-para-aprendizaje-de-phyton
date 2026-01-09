90e67624c5e8a64cc9b1839e0c9025d27f655654
Raul <rvilara@gmail.com>
2026-01-09 19:55:37 +0100

    primer commit

---
+++ primer programa.py
@@ -0,0 +1,15 @@
+print("Hola, este es mi primer programa")
+
+horas = 3
+precio = 15
+
+total = horas * precio
+
+print("El total es:", total)
+
+if total > 50:
+    print("Es un importe alto")
+if total < 50:
+    print("Es barato")
+if total == 50:
+    print("esta coorecto")
